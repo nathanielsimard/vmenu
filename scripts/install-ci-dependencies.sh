c547f7e99c67db471365d3eb045144aaa7ba0313
nathaniel <nathaniel.simard.42@gmail.com>
2020-01-01 13:25:01 -0500

    Add .local/bin to path

--- scripts/install-ci-dependencies.sh
+++ scripts/install-ci-dependencies.sh
@@ -4,4 +4,5 @@ sudo apt-get install wget vim
 wget https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage
 chmod +x nvim.appimage
 sudo mv nvim.appimage /usr/bin/nvim
+export PATH="$HOME/.local/bin:$PATH"
 
