ae2c3fe16e8d8802f2eb5c61fc2e97cad24c1ae2
nathaniel <nathaniel.simard.42@gmail.com>
2020-01-01 13:35:33 -0500

    Cleanup

--- scripts/install-ci-dependencies.sh
+++ scripts/install-ci-dependencies.sh
@@ -1,8 +1,21 @@
 #!/bin/bash
 
-sudo apt-get install wget vim
+# Installing Vint
+pip install vim-vint
+
+# Installing Vim
+sudo apt-get install vim
+
+# Installing Neovim
+sudo apt-get install wget
 wget https://github.com/neovim/neovim/releases/download/v0.4.3/nvim.appimage
 chmod +x nvim.appimage
 sudo mv nvim.appimage /usr/bin/nvim
-export PATH="$HOME/.local/bin:$PATH"
+
+# Installing VUnit
+git clone https://github.com/nathanielsimard/vunit
+cd vunit
+sudo ./install.sh /usr
+cd ..
+rm -rf vunit
 
